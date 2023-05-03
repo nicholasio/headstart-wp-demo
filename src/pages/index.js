1ef89cf98859f631a34ca8dccc16220eb999d141
Nícholas André <nicholas.andre@10up.com>
2023-05-03 00:55:02 +0000

    step:1 creating index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,40 @@
+import {
+	addHookData,
+	fetchHookData,
+	handleError,
+	useAppSettings,
+	usePost,
+} from '@10up/headless-next';
+import { resolveBatch } from '../utils/promises';
+
 const Homepage = () => {
-	return <h1>HeadstartWP - 10up Summit 2023</h1>;
+	const { data, loading } = usePost({ slug: 'front-page', postType: 'page' });
+
+	if (loading) {
+		return 'Loading...';
+	}
+
+	return <h1>{data.post.title.rendered}</h1>;
 };
 
 export default Homepage;
+
+export async function getStaticProps(ctx) {
+	try {
+		const settledPromises = await resolveBatch([
+			{
+				func: fetchHookData(usePost.fetcher(), ctx, {
+					params: { slug: 'front-page', postType: 'page' },
+				}),
+			},
+			{
+				func: fetchHookData(useAppSettings.fetcher(), ctx),
+				throw: false,
+			},
+		]);
+
+		return addHookData(settledPromises, {});
+	} catch (e) {
+		return handleError(e);
+	}
+}
